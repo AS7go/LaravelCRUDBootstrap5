ba189a4a434c180121f7218f11e5f1749f9e7dbf
Aleksandr <sh17aleksandr@gmail>
2024-01-19 17:24:34 +0200

    paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::get();
+        // $users = User::get();
+        $users = User::paginate();
         return view('index', compact('users'));
     }
 
