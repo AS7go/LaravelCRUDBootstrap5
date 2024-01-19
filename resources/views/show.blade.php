fa93279c1273a1bb9ab183ef74eb11226ae1fafd
Aleksandr <sh17aleksandr@gmail>
2024-01-19 09:04:09 +0200

    delete action

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,15 +6,22 @@
 <a type="button" class="btn btn-secondary" href="{{ route('users.index') }}">Back to users</a>
 
 <div class="card mt-3" style="width: 18rem;">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Id: {{ $user->id }}</li>
-    <li class="list-group-item">Name: {{ $user->name }}</li>
-    <li class="list-group-item">Email: {{ $user->email }}</li>
-    <li class="list-group-item">Created: {{ $user->created_at->format('d/m/y H:i:s') }}</li>
-    <li class="list-group-item">Updated: {{ $user->updated_at->format('d/m/y H:i:s') }}</li>
-  </ul>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Id: {{ $user->id }}</li>
+        <li class="list-group-item">Name: {{ $user->name }}</li>
+        <li class="list-group-item">Email: {{ $user->email }}</li>
+        <li class="list-group-item">Created: {{ $user->created_at->format('d/m/y H:i:s') }}</li>
+        <li class="list-group-item">Updated: {{ $user->updated_at->format('d/m/y H:i:s') }}</li>
+    </ul>
 </div>
-<a type="button" class="btn btn-warning mt-3" href="{{ route('users.edit', $user) }}">Edit</a>
+<form method="POST" class="mt-3"    action="{{ route('users.destroy', $user) }}">
+    <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
+    @csrf
+    @method('DELETE')
+
+    <button type="submit" class="btn btn-danger">Delete</button>
+</form>
+<!-- <a type="button" class="btn btn-warning mt-3" href="{{ route('users.edit', $user) }}">Edit</a> -->
 
 
 @endsection
